c0077dafa9a3efc360d339a9d9c6725079b6950a
omjadas <omja.das@gmail.com>
2019-09-08 18:21:28 +1000

    added source files

---
+++ Proj1.hs
@@ -0,0 +1,6 @@
+--  File     : Proj1.hs
+--  Author   : Omja Das
+--  Purpose  : An implementation of a card guessing game
+
+module Proj1 (feedback, initialGuess, nextGuess, GameState) where
+    import Card
