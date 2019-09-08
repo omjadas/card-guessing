27ff3ee6d27d6626e752aea7cf295c3c986938d4
omjadas <omja.das@gmail.com>
2019-09-08 18:31:15 +1000

    added scaffolding

--- Proj1.hs
+++ Proj1.hs
@@ -3,4 +3,13 @@
 --  Purpose  : An implementation of a card guessing game
 
 module Proj1 (feedback, initialGuess, nextGuess, GameState) where
+
     import Card
+
+    data GameState
+
+    feedback :: [Card] -> [Card] -> (Int, Int, Int, Int, Int)
+
+    initialGuess :: Int -> ([Card], GameState)
+
+    nextGuess :: ([Card], GameState) -> (Int, Int, Int, Int, Int) -> ([Card], GameState)
